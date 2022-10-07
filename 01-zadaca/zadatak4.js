e316adc0e9b99cccee12700af36d57e5991c46bf
cervar <75136644+cervar@users.noreply.github.com>
2022-10-07 19:31:44 +0200

    rješen zadatak 4

--- 01-zadaca/zadatak4.js
+++ 01-zadaca/zadatak4.js
@@ -2,3 +2,14 @@
 /*4. Napiši funkciju koja pretvara broj X u sate i minute
 Ispis : 120 -> “Ovo je 2 sata i 0 minuta” */
 
+let convertToHours = (minutes) => {
+    let hours = Math.floor(minutes / 60)
+    let remainingMinutes = minutes % 60
+    return hours + " hrs, " + remainingMinutes + " mins"
+  }
+  
+  console.log(convertToHours(120)) // 2 hrs, 0 mins
+  console.log(convertToHours(180)) // 3 hrs, 0 mins
+  console.log(convertToHours(44)) // 0 hrs, 44 mins
+  console.log(convertToHours(60)) // 1 hrs, 0 mins
+  
