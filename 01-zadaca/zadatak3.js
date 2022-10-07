3a87fd7861b51926d413b16b6024dce3d85be4fa
cervar <75136644+cervar@users.noreply.github.com>
2022-10-07 19:28:59 +0200

    rješen zadatak 3

--- 01-zadaca/zadatak3.js
+++ 01-zadaca/zadatak3.js
@@ -2,3 +2,10 @@
 Ispis : 53 -> “53 nije unutar [100,150000]” */
 
 
+let check = (num) => (num >= 100 && num <= 150000 ? true : false)
+
+console.log(check(100)) // true
+console.log(check(69)) // false
+console.log(check(149999)) // true
+console.log(check(666)) // true
+console.log(check(696969)) // false
