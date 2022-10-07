ffd886919e12666417c3b3ba7ee3a2e4bf86242f
cervar <75136644+cervar@users.noreply.github.com>
2022-10-07 19:37:59 +0200

    rjesen zadatak 6

--- 01-zadaca/zadatak6.js
+++ 01-zadaca/zadatak6.js
@@ -2,3 +2,17 @@
 /*6. Napiši funkciju kojoj se predaje string, te ona ispisuje bez razmaka svaku
 rijec velikim slovom (cammelCase)
 Ispis : “web apps vjezbe” -> “webAppsVjezbe”*/
+
+
+let camelCase = (x) => {
+    wordArr = x.split(" ")
+    for (let i = 0; i < wordArr.length; i++) {
+      if (i === 0) continue
+      else {
+        wordArr[i] = wordArr[i][0].toUpperCase() + wordArr[i].substring(1)
+      }
+    }
+    return wordArr.join("")
+  }
+  
+  console.log(camelCase("web apps vjezbe")) // webAppsVjezbe
