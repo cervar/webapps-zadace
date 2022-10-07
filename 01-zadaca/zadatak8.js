8071aef77a4916659524b5e90e17e62b9b54864b
cervar <75136644+cervar@users.noreply.github.com>
2022-10-07 19:41:04 +0200

    rješen zadatak 8

--- 01-zadaca/zadatak8.js
+++ 01-zadaca/zadatak8.js
@@ -1,3 +1,10 @@
 
 /*8. Napiši funkciju koja vraća predano polje obrnutim redosljedom
 Ispis : [3,4,5,6] -> [6,5,4,3]*/
+
+
+let reverse = (arr) => {
+    return arr.reverse()
+  }
+  
+  console.log(reverse([3, 4, 5, 6])) // [6, 5, 4, 3]
