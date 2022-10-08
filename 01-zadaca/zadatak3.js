50b70368cbae46d0caf88352106ef36229d2cb13
cervar <75136644+cervar@users.noreply.github.com>
2022-10-08 18:50:02 +0200

    ispravak zadatak 3

--- 01-zadaca/zadatak3.js
+++ 01-zadaca/zadatak3.js
@@ -2,10 +2,40 @@
 Ispis : 53 -> “53 nije unutar [100,150000]” */
 
 
-let check = (num) => (num >= 100 && num <= 150000 ? true : false)
 
-console.log(check(100)) // true
-console.log(check(69)) // false
-console.log(check(149999)) // true
-console.log(check(666)) // true
-console.log(check(696969)) // false
+
+
+function numbers_ranges(x,) {
+    if (x >= 100 && x <= 150000) 
+        
+       {
+      return true;
+       } 
+      else 
+       {
+      return false;
+    }
+  }
+  
+  console.log(numbers_ranges(100)); //true
+  console.log(numbers_ranges(70)); // false
+  console.log(numbers_ranges(69524)); //true
+  
+
+// funkcija ako imamo 2 broja x i y 
+  function numbers_ranges(x, y) {
+    if ((x >= 100 && x <= 150000 && y >= 100 && y <= 150000) 
+        || 
+        (x >= 100 && x <= 150000 && y >= 100 && y <= 150000))
+       {
+      return true;
+       } 
+      else 
+       {
+      return false;
+    }
+  }
+  
+  console.log(numbers_ranges(99, 8)); // false
+  console.log(numbers_ranges(666, 555)); // true
+  console.log(numbers_ranges(149999, 5205)); //true
