aecd9453e00b9d390c023f74a077319294926b12
cervar <75136644+cervar@users.noreply.github.com>
2022-10-11 16:40:20 +0200

    ispravak zadatak 6

--- 01-zadaca/zadatak6.js
+++ 01-zadaca/zadatak6.js
@@ -16,3 +16,14 @@ let camelCase = (x) => {
   }
   
   console.log(camelCase("web apps vjezbe")) // webAppsVjezbe
+// novi kod
+
+
+  function camelize(str) {
+    return str.replace(/(?:^\w|[A-Z]|\b\w)/g, function(word, index) {
+      return index === 0 ? word.toLowerCase() : word.toUpperCase();
+    }).replace(/\s+/g, '');
+  }
+  
+  camelize("web apps vjezbe");
+ 
